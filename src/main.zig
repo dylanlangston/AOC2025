468c7f3daee5a53652a0cb7d3552be5dae8618fa
Dylan <dylanlangston@gmail.com>
2025-12-02 04:18:42 +0000

    Day 1

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
 const day_one = @import("days/one.zig");
 
-pub fn main() !void {}
+pub fn main() !void {
+    const day1_part1 = try day_one.Solution_Part_One();
+    std.debug.print("Solution to Day 1, Part 1: {d}\n", .{day1_part1});
 
-test {}
+    const day1_part2 = try day_one.Solution_Part_Two();
+    std.debug.print("Solution to Day 1, Part 2: {d}\n", .{day1_part2});
+}
+
+test {
+    _ = day_one;
+}
