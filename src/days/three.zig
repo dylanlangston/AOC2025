c73e5fa96e526a41c924c0459868ed391710b393
Dylan <dylanlangston@gmail.com>
2025-12-03 05:11:54 +0000

    Placeholder for Day 3 with example pattern test

---
+++ src/days/three.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+const examplePattern1 = "987654321111111,811111111111119,234234234234278,818181911112111";
+
+test "Example Pattern Part 1" {
+    const totalJoltage = null;
+    try std.testing.expectEqual(357, totalJoltage);
+}
+
+// pub fn Solution_Part_One() !u64 {
+
+// }
+
+// test "Solution Part One" {
+
+// }
