84105ff17b435a2ba7dd9c5b248ea0d389e1e8cc
Dylan <dylanlangston@gmail.com>
2025-12-04 01:21:29 +0000

    Placeholders for remaining days of AOC

---
+++ src/days/six.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+const root = @import("AOC2025");
+const inputs = @import("input").Input;
+const solutions = root.Solutions;
+
+const examplePattern1 =
+    \\
+;
+const examplePattern2 = "";
+const finalPattern = inputs.six.data();
+
+test "Example Pattern Part 1" {}
+
+test "Minimal Example Pattern Part 1" {}
+
+// pub fn Solution_Part_One() !u64 {
+
+// }
+
+// test "Solution Part One" {
+//     const result = try Solution_Part_One();
+//     try std.testing.expectEqual(solutions.DaySix_PartOne.value(), result);
+// }
+
+test "Example Pattern Part 2" {}
+
+test "Minimal Example Pattern Part 2" {}
+
+// pub fn Solution_Part_Two() !u64 {
+
+// }
+
+// test "Solution Part Two" {
+//     const result = try Solution_Part_Two();
+//     try std.testing.expectEqual(solutions.DaySix_PartTwo.value(), result);
+// }
